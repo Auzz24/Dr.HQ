96162b90ab52a93d9eea153df61bbf0e6a4f60c7
RCSchornick <94027300+RCSchornick@users.noreply.github.com>
2022-04-07 05:15:50 -0500

    starting React framework

---
+++ dr-hq/src/App.js
@@ -0,0 +1,29 @@
+import Home from './Components/Home.js';
+import Header from './Components/Header.js';
+import Footer from './Components/Footer.js';
+import Department from './Components/Department';
+import {BrowserRouter as Router,  Route} from "react-router-dom";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Router>
+      <Header/>
+        {/* <Switch> */}
+          <Route exact path="/Home" component={Home}/>
+          <Route exact path="/Department" component={Department}/>
+          {/* <Route exact path="/Login" component={Login}/> */}
+          <Route exact path="/" component={Home}/>
+        {/* </Switch> */}
+        <Footer/>
+      </Router>
+     
+    </div>
+  );
+}
+
+export default App;
