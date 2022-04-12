3a3e1cd7d94dd2fa898d5f860aff5394fe469dd5
Austin Miller <austin.miller1424@gmail.com>
2022-04-12 09:35:56 -0500

    start of homepage, made and linked find a doctor button

--- dr-hq/src/Components/Home.js
+++ dr-hq/src/Components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Parallax} from 'react-scroll-parallax';
 import {Row, Col} from "reactstrap";
+import {Link} from 'react-router-dom';
 import "./Style/Components.css";
 
 
@@ -8,11 +9,27 @@ import "./Style/Components.css";
 
 function Home() {
   return (
-
+ 
       <div bgImage={require("./Style/lobby2.jpg")}>
           <div style={{ height: 1000 }}>
-            
-          </div>
+            <div id="jumbo">
+              <h3>
+                The Dr.HQ team is here to make finding a doctor easy. We put your health first.
+                </h3>
+              <p>
+              seitan heirloom post-ironic pop-up iPhone mlkshk hella selfies fashion axe occupy readymade put a bird on it
+          messenger bag Wes Anderson Schlitz plaid Bushwick church-key lo-fi skateboard slow-carb hashtag trust fund
+          Williamsburg biodiesel fixie farm-to-table 8-bit banjo XOXO Banksy chillwave bicycle rights retro cliche
+          tattooed bespoke irony mumblecore Shoreditch deep v polaroid McSweeney's bitters 
+              </p>
+              <button>
+                <Link to="/Doctor">
+                  FIND YOUR DOCTOR HERE!
+                </Link>
+              </button>
+            </div>
+
+      </div>
     </div>
   )
 }
