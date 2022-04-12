aff014e0f49afd36a1ae862518c3cf169ba025e2
Nehailaa <93628865+Nehailaa@users.noreply.github.com>
2022-04-11 23:08:21 -0500

    navbar & aboutUs & contactUs

---
+++ dr-hq/src/Components/AboutUs.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function About() {
+   return (
+      <section id="about">
+         <div class="bgimg">
+            <h1 class="display-1 "></h1>
+            <hr></hr>
+            <img src="" class="rounded-circle mx-auto d-block" width={400} alt="Dr-HQ" />
+            <div class="container about-section">
+               <p class="blockquote">ADD SOMETHING ABOUT US</p>
+               
+            </div>
+         </div>
+      </section>
+   );
+}
---
+++ dr-hq/src/Components/Portgenerator.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import Navbar from './Components/Navbar';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import About from './Components/AboutMe';
+import ContactUs from './Components/ContactUs';
+
+export default function CurrentPortfolio() {
+    const [currentPage, setCurrentPage] = useState('Home');
+
+    const renderPage = () => {
+        switch (currentPage) {
+            case 'Home':
+                return <Home />;
+            case 'About':
+                return <About />;
+            case 'Test1':
+                return <Tes1 />;
+            case 'ContactUs':
+                return <ContactUs/>;
+            case 'Test2':
+                return <Test2 />;
+            default:
+                console.log('default switch case has been provoked!');
+        }
+
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+            <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+            {renderPage()}
+        </div>
+    );
+}
