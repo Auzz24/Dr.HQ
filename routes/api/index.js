ba00e347a6626f9281243c631ab6510304bffec9
Austin Miller <austin.miller1424@gmail.com>
2022-04-19 21:25:37 -0500

    homepage megre to main

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 // const departmentRoutes = require('./department-routes');
-const doctorRoutes = require ('./doctor-routes');
+const doctorRoutes = require ('./doctor-routes'); 
 
 // router.use('/department', departmentRoutes);
 router.use('/doctor', doctorRoutes)
