6f84533f60c641c85972b1c4c345c314078d5705
Austin Miller <austin.miller1424@gmail.com>
2022-04-26 20:17:41 -0500

    package,json fix

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const mongoose = require('mongoose');
 
-const { typeDefs, resolvers } = require('./server/schemas');
-const { authMiddleware } = require('./server/utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 // const db = require('./config/connection');
 
 const app = express();
